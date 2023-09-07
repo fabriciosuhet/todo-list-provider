e4fa3be5db52f99479f0af5fd6bbdfd27f738f80
fabriciosuhet <fabriciosuhet@hotmail.com>
2023-09-07 18:06:03 -0300

    Alterando nome do usuário

---
+++ lib/app/modules/home/widgets/home_drawer.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_overlay_loader/flutter_overlay_loader.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/core/auth/auth_provider.dart';
+import 'package:todo_list_provider/app/core/ui/messages.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/services/user/user_service.dart';
+
+class HomeDrawer extends StatelessWidget {
+  final nameVN = ValueNotifier<String>('');
+
+  HomeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: context.primaryColor.withAlpha(70),
+            ),
+            child: Row(
+              children: [
+                Selector<AuthProvider, String>(
+                    selector: (context, authProvider) {
+                  return authProvider.user?.photoURL ??
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png';
+                }, builder: (_, value, __) {
+                  return CircleAvatar(
+                    backgroundImage: NetworkImage(value),
+                    radius: 30,
+                  );
+                }),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Selector<AuthProvider, String>(
+                      selector: (context, authProvider) {
+                        return authProvider.user?.displayName ??
+                            'Não Informado';
+                      },
+                      builder: (_, value, __) {
+                        return Text(
+                          value,
+                          style: context.textTheme.titleMedium,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (_) {
+                    return AlertDialog(
+                      title: const Text('Alterar nome'),
+                      content: TextField(
+                        onChanged: (value) => nameVN.value = value,
+                      ),
+                      actions: [
+                        TextButton(
+                            onPressed: () => Navigator.of(context).pop(),
+                            child: const Text(
+                              'Cancelar',
+                              style: TextStyle(color: Colors.red),
+                            )),
+                        TextButton(
+                            onPressed: () async {
+                              final nameValue = nameVN.value;
+                              if (nameValue.isEmpty) {
+                                Messages.of(context)
+                                    .showError('Nome obrigatório');
+                              } else {
+                                Loader.show(context);
+                                await context
+                                    .read<UserService>()
+                                    .updateDisplayName(nameValue);
+                                Loader.hide();
+                                if (!context.mounted) return;
+                                Navigator.of(context).pop();
+                              }
+                            },
+                            child: const Text('Alterar'))
+                      ],
+                    );
+                  });
+            },
+            title: const Text('Alterar nome'),
+          ),
+          ListTile(
+            onTap: () => context.read<AuthProvider>().logout(),
+            title: const Text('Sair'),
+          ),
+        ],
+      ),
+    );
+  }
+}
