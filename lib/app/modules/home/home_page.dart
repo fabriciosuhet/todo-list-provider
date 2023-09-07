e01f7f1f13dc0ff4c51d13ab249f92d8687ef1e2
fabriciosuhet <fabriciosuhet@hotmail.com>
2023-09-07 20:27:02 -0300

    Construindo layout da HomePage

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list_provider/app/core/auth/auth_provider.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_icons.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_filters.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,10 +12,48 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        iconTheme: IconThemeData(color: context.primaryColor),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(TodoListIcons.filter),
+            itemBuilder: (_) => [
+              const PopupMenuItem<bool>(
+                child: Text('Mostrar tarefas concluidas.'),
+              ),
+            ],
+          ),
+        ],
       ),
+      backgroundColor: const Color(0xFFFAFBFE),
       drawer: HomeDrawer(),
-      body: Container(),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxHeight,
+              ),
+              child: Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: const IntrinsicHeight(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      HomeHeader(),
+                      HomeFilters(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
